e952b525f434bfe326413b1133479e4163665886
Galileo Sartor <galileo.sartor@gmail.com>
2024-12-20 11:18:35 +0100

    Add included LE pack and docker

--- Syntax.dockerfile
+++ Syntax.dockerfile
@@ -1,13 +1,7 @@
-FROM ubuntu:latest
-
-ENV DEBIAN_FRONTEND=noninteractive
-
-RUN apt-get update && apt-get install -y software-properties-common \
-    && apt-add-repository ppa:swi-prolog/stable \
-    && apt-get install -y swi-prolog-nox --no-install-recommends
-
+FROM swipl:latest
 COPY . .
 
-RUN swipl pack install -y logicalenglish-0.0.3.zip
+# RUN swipl pack install -y logicalenglish-1.0.zip
 
+# WORKDIR /app
 CMD ["swipl", "parse_le.pl"]
--- parse_le.pl
+++ parse_le.pl
@@ -1,16 +1,14 @@
-:- use_module(library(le_answer)).
+% :- use_module(library(le_answer)).
 
 :- initialization(main, main).
 
 main :-
+    consult('logicalenglish/prolog/le_answer.pl'),
     read_file_to_string('traffic_rules.le', String, []),
-    % string_lines(String, L),
-    % find('scenario 'S, L),
-    % find('query ', Q, L),
-    parse_and_query_and_explanation('traffic_rules', en(String), null, with(null), _).
+    once(parse_and_query('traffic_rules', en(String), enter, with(car), _)).
 
-find(Text, S, L) :-
-    member(X, L),
-    atom_concat(Text, Y, X),
-    atom_concat(Test, "is:", Y),
-    !.
+% find(Text, S, L) :-
+%     member(X, L),
+%     atom_concat(Text, Y, X),
+%     atom_concat(Test, "is:", Y),
+%     !.
