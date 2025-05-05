c3cf14f1fe4025bc70342fa1b42fe2f63b5fd251
Galileo Sartor <galileo.sartor@gmail.com>
2025-05-05 14:27:46 +0200

    Remove sumo simulator

--- sumo_control/README.md
+++
@@ -1,4 +0,0 @@
-<!-- netgenerate option --no-turnarounds -->
-
-To generate from a osm map:
-- 
--- sumo_control/traffic_rules.pl
+++
@@ -1,6 +0,0 @@
-:- style_check(-singleton).
-:- style_check(-discontiguous).
-
-:- op(900,fx,user:not). % same as \+
-
-:- use_module('traffic_rules-prolog.pl').
