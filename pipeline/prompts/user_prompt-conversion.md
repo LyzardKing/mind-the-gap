c0f331f0e8483c60fcd9e96386c3bf8accef8928
Thiago Raulino Dal Pont <thiagordalpont@gmail.com>
2025-07-01 17:35:38 +0200

    Structuring

--- pipeline/prompts/user_prompt-conversion.md
+++ pipeline/prompts/user_prompt-conversion.md
@@ -1,5 +1,7 @@
 ## Sentence to Analyse
+
 @SOURCE_TEXT
     
 ## Previous set of templates
+
 @PREVIOUS_TEMPLATES
--- pipeline/rule_generation.py
+++ pipeline/rule_generation.py
@@ -2,6 +2,28 @@
 # Rule Generation
 # 
 
-def translate_to_le(source_text:str) -> str:
+def translate_to_le(source_text:str, previous_le:str) -> str:
+
+    #1 Fill the user prompt with the source text and previous LE
+    #2 Send to the API
+    #3 Get the generate LE.
+    #4 While not validated:
+    #4.1 Send to LE compiler
+    #4.2 If error, get the error description
+    #4.2.1 Fill the user propmt for the syntax check
+    #4.3 Generate again.
+
     pass
 
+
+
+
+if __name__ == "__main__":
+    source_text_sample = open("source_text/Rule176.txt").read().strip()
+    previous_logical_english= "??"
+
+
+    translate_to_le(
+        source_text=source_text_sample,
+        previous_le=previous_logical_english
+    )
